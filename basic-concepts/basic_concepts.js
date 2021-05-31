d549ed48a34e1efe44019ad16f5f3e90991874bc
Viraj Nimbalkar <virajnimbalkar55@gmail.com>
2021-05-31 18:16:53 +0530

    Truthly and falsy values in JS

--- basic-concepts/basic_concepts.js
+++ basic-concepts/basic_concepts.js
@@ -107,7 +107,7 @@ template literal`);
 //Typer conversion
 const inputYear = "1991";
 console.log("inputYear", inputYear + 10); // it will log 199110 cause we take input as a string
-console.log("inputYear convversion", Number(inputYear) + 10); // it will print 2001 cause we converted string to number
+console.log("inputYear conversion", Number(inputYear) + 10); // it will print 2001 cause we converted string to number
 console.log("both cases", Number(inputYear), inputYear);
 
 console.log("NaN", Number("String"));
@@ -122,3 +122,25 @@ console.log("using + in string", "10" + "3" + "2");
 console.log("using * in string", "10" * "3" * "2");
 console.log("using / in string", "10" / "3" / "2");
 console.log("using comparison in string", "10" > "1");
+
+//There are only 5 falsy vallues in JS
+// Falsy values: The values that we cannot convert in other form
+// There values will become false when converted to boolean
+console.log("boolean value of 0", Boolean(0));
+console.log("boolean value of undefined", Boolean(undefined));
+console.log("boolean value of null", Boolean(null));
+console.log("boolean value of NaN", Boolean(NaN));
+console.log("boolean value of", Boolean(""));
+
+console.log("boolean value of String", Boolean("String"));
+console.log("boolean value of {}", Boolean({}));
+
+// Bug
+// Though we defined height below example will run else loop cause of value 0 and this is a bug
+
+let height = 0;
+if (height) {
+  console.log("Not a bug");
+} else {
+  console.log("Its a bug");
+}
