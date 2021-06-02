cf44b943108deffa5d8412b9e2d5ac0017564dbb
Viraj Nimbalkar <virajnimbalkar55@gmail.com>
2021-06-02 14:07:58 +0530

    Function declaration and function expressions in JS

--- basic-concepts/basic_concepts_2/basic_concepts_2.js
+++ basic-concepts/basic_concepts_2/basic_concepts_2.js
@@ -13,3 +13,38 @@ if (hasDriverLicense) {
 }
 
 const interface = "JS feature"; // we cannot use certain variables which are same as JS features.
+
+//Functions
+function logger() {
+  console.log("First function");
+}
+logger();
+logger();
+
+function fruitProcessor(apples, oranges) {
+  console.log("apples, oranges:", apples, oranges);
+  const juice = `Juice with ${apples} apples and ${oranges} oranges`;
+  return juice; // we returned a value here hence this function has some value that we used in below example
+}
+const appleJuice = fruitProcessor(5, 2); // value of returned juice is stored here in function and if we want that value we can store function in variable
+console.log(appleJuice);
+console.log(fruitProcessor(5, 2)); // It is same as above
+
+const appleOrangeJuice = fruitProcessor(2, 8);
+console.log(appleOrangeJuice);
+
+//Function declaration and function expression
+
+// 1. Function declaration:
+function calcAge(birthYear) {
+  return 2021 - birthYear;
+}
+const age = calcAge(1990);
+console.log("Function declaration:", age);
+
+// 2. Function expression:
+const calcAge2 = function (birthYear) {
+  return 2021 - birthYear;
+};
+const age2 = calcAge2(1990);
+console.log("Function expression:", age2);
