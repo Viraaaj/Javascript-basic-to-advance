d127b1322d55e0dc0e46d315766df66fa3b42c35
Viraj Nimbalkar <virajnimbalkar55@gmail.com>
2021-06-03 20:49:57 +0530

    Object methods in JS

--- data_structures/objects/objects.js
+++ data_structures/objects/objects.js
@@ -51,3 +51,50 @@ console.log("After adding data:", object);
 console.log(
   `${object.firstName} has ${object.friends.length} friends, and his best friend is called ${object.friends[0]} `
 );
+
+//Object methods
+const objectMethods = {
+  firstName: "John",
+  hasLicense: true,
+  profession: "developer",
+  birthYear: 1990,
+  friends: ["Natasha", "friend2", "friend3", "friend4"],
+  calcAge: function (birthYear) {
+    return 2021 - birthYear;
+  }, //Method 1
+
+  calcAge1: function () {
+    return 2021 - this.birthYear;
+  }, //Method 2
+
+  calcAge2: function () {
+    this.age = 2021 - this.birthYear; //creating new property called age and storing value in that varible
+    return this.age;
+  }, //Method 3
+
+  getSummery: function () {
+    return `${this.firstName} is a ${this.calcAge2()}-years-old ${
+      this.profession
+    }, and he has ${this.hasLicense ? "a" : "no"} drivers license`;
+  }, //Challenge
+};
+console.log("Object:", objectMethods);
+console.log(
+  "Function inside object method 1:",
+  objectMethods.calcAge(objectMethods.birthYear)
+);
+console.log("Function inside object method 2:", objectMethods.calcAge1());
+console.log("Function inside object method 3:", objectMethods.calcAge2()); // we need to call function before executing below line
+console.log("Function inside object method 3:", objectMethods.age);
+
+//challenge
+// if (objectMethods.hasLicense) {
+//   console.log(
+//     `${objectMethods.firstName} is a ${objectMethods.age}-years-old ${objectMethods.profession}, and he has a drivers license`
+//   );
+// } else {
+//   console.log(
+//     `${objectMethods.firstName} is a ${objectMethods.age}-years-old ${objectMethods.profession}, and he dont have a drivers license`
+//   );
+// }
+console.log(objectMethods.getSummery());
