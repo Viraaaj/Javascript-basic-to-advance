bb325e97a634add6bffa78f926b228aeb6a30de5
Viraj Nimbalkar <virajnimbalkar55@gmail.com>
2021-06-08 19:31:09 +0530

    Completed game, click events, handling css with help of JS, refactoring code

--- guess_the_number/guess_the_number.js
+++ guess_the_number/guess_the_number.js
@@ -13,45 +13,64 @@
 // document.querySelector(".guess").value = 20;
 
 //random number
-const randomNumber = Math.trunc(Math.random() * 20) + 1;
+let randomNumber = Math.trunc(Math.random() * 20) + 1;
 
 //wrong input score change
-let wrongGuessScore = 20;
+let guessScore = 10;
+let highscore = 0;
 
-document.querySelector(".number").textContent = randomNumber;
+//refactoring
+const displayMessage = function (message) {
+  document.querySelector(".message").textContent = message;
+}; // Like this we can refactor code for all other values by creating seperate function
 
 //Event listners
 document.querySelector(".check").addEventListener("click", function () {
   const inputNumber = Number(document.querySelector(".guess").value);
   console.log(inputNumber);
 
+  //No input
   if (!inputNumber) {
-    document.querySelector(".message").textContent = "Invalid input!!";
+    displayMessage("Invalid input!!");
     console.log("Null value");
-  } else if (inputNumber === randomNumber) {
-    document.querySelector(".message").textContent = "Correct Number!";
-  } else if (inputNumber > randomNumber) {
-    //change text won or loss
-    if (wrongGuessScore > 1) {
-      document.querySelector(".message").textContent = "Too High!";
-      //change score
-      wrongGuessScore = wrongGuessScore - 1;
-      document.querySelector(".score").textContent = wrongGuessScore;
-    } else {
-      document.querySelector(".message").textContent = "You lost the game!";
-      document.querySelector(".score").textContent = 0;
-    }
-  } else if (inputNumber < randomNumber) {
-    //change text won or loss
-    if (wrongGuessScore > 1) {
-      document.querySelector(".message").textContent = "Too Low!";
+  }
 
+  //Win
+  else if (inputNumber === randomNumber) {
+    displayMessage("Correct Number!");
+    document.querySelector("body").style.backgroundColor = "#663399";
+    document.querySelector(".number").style.width = "30rem";
+    document.querySelector(".message").style.color = "#00FA9A";
+    document.querySelector(".guess").style.color = "#00FA9A";
+    document.querySelector(".number").textContent = randomNumber;
+    if (guessScore > highscore) {
+      highscore = guessScore;
+      document.querySelector(".highscore").textContent = highscore;
+    }
+  } else if (inputNumber !== randomNumber) {
+    //change text won or loss
+    if (guessScore > 1) {
+      displayMessage(inputNumber > randomNumber ? "Too High!" : "Too Low!");
       //change score
-      wrongGuessScore = wrongGuessScore - 1;
-      document.querySelector(".score").textContent = wrongGuessScore;
+      guessScore = guessScore - 1;
+      document.querySelector(".score").textContent = guessScore;
     } else {
-      document.querySelector(".message").textContent = "You lost the game!";
+      displayMessage("You lost the game!");
       document.querySelector(".score").textContent = 0;
     }
   }
+});
+
+//Reset button
+document.querySelector(".again").addEventListener("click", function () {
+  displayMessage("Start guessing...");
+  document.querySelector("body").style.backgroundColor = "#222";
+  document.querySelector(".number").style.width = "15rem";
+  document.querySelector(".message").style.color = "#eee";
+  document.querySelector(".guess").style.color = "#eee";
+  document.querySelector(".guess").value = "";
+  document.querySelector(".number").textContent = "?";
+  document.querySelector(".score").textContent = 10;
+  guessScore = 10;
+  randomNumber = Math.trunc(Math.random() * 20) + 1;
 });
