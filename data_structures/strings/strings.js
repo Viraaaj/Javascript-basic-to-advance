31d93b42f6b78ecf73bbdf64a5cd8881c17508ad
Viraj Nimbalkar <virajnimbalkar55@gmail.com>
2021-06-24 14:20:29 +0530

    Strings: Methods, properties part 2 in JS

--- data_structures/strings/strings.js
+++ data_structures/strings/strings.js
@@ -46,3 +46,87 @@ console.log(
   "Behind the scenes string type when sliced | automatically converted:",
   typeof new String("Hello").slice(1)
 );
+
+console.log(
+  "-------------------------Part 2----------------------------------------"
+);
+
+const uppercase = "UPPERCASE";
+console.log("Converting string to lower case:", uppercase.toLowerCase());
+
+const lowercase = "lowercase";
+console.log(
+  "Converting string to uppercase case:",
+  lowercase.toLocaleUpperCase()
+);
+
+//Fix name
+const passengerName = "jOhN"; //John
+const passengerLowercase = passengerName.toLowerCase();
+const firstCapital =
+  passengerLowercase[0].toUpperCase() + passengerLowercase.slice(1);
+
+console.log(`${passengerName} is coonverted to ${firstCapital}`);
+
+//check email
+const email = "john@email.com";
+const wrongEmail = "    John@email.com ";
+console.log("Wrong email is:", wrongEmail);
+
+const emailLowercase = wrongEmail.toLowerCase();
+const trimmedEmail = emailLowercase.trim();
+console.log("Trimmed email without space and uppercase letters:", trimmedEmail);
+
+const trimMethod2 = wrongEmail.toLowerCase().trim();
+console.log("Using multiple string methods:", trimMethod2);
+
+//replacing
+const price = "10,000#";
+const priceUS = price.replace("#", "$").replace(",", ".");
+console.log("replacing element in string:", priceUS);
+
+const annoucement = "All doors are open except back doors";
+console.log(
+  "replacing doors with gates:",
+  annoucement.replace("doors", "gates")
+); //It will only replace the first occurance
+console.log(
+  "replacing all doors with gates:",
+  annoucement.replaceAll("doors", "gates")
+);
+console.log(
+  "replacing all doors with gates using regular expressions:",
+  annoucement.replace(/doors/g, "gates")
+);
+
+//Booleans
+const methodBoolean = "A320neo";
+console.log("String is:", methodBoolean);
+console.log("checking includes:", methodBoolean.includes("A32"));
+console.log(
+  "checking if string starts with A32:",
+  methodBoolean.startsWith("A32")
+);
+console.log(
+  "checking if string starts with yooo:",
+  methodBoolean.startsWith("yooo")
+);
+console.log("checking if string ends with neo:", methodBoolean.endsWith("neo"));
+console.log(
+  "checking if string ends with boom:",
+  methodBoolean.endsWith("boom")
+);
+
+//Practise Exercise
+const checkBaggage = function (items) {
+  const baggage = items.toLowerCase();
+  if (baggage.includes("knife") || baggage.includes("gun")) {
+    console.log(`${items} : You are carrying some wild stuff`);
+  } else {
+    console.log(`${items} : Happy Journey!!`);
+  }
+};
+checkBaggage("I have Laptop, Food and Knife");
+checkBaggage("I have socks, food and camera");
+checkBaggage("I have snacks, gun and water");
+//It is a good practise to first coonvert a string into lowecase if we want to test some words, coz strings are case sensitive. Y !== y
